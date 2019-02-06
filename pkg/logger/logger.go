8940a94e913261f3883510c6a761ac674eb4664b
Sam Mulube <sam@umbrellium.co.uk>
2019-02-06 16:35:13 +0000

    Add a couple of functions to get/set logger from context

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,14 +1,37 @@
 package logger
 
 import (
+	"context"
 	"os"
 
 	kitlog "github.com/go-kit/kit/log"
 	"github.com/thingful/kuzu/pkg/version"
 )
 
+type contextKey string
+
+const (
+	loggerKey = contextKey("logger")
+)
+
 // NewLogger returns a new kitlog.Logger instance ready for use.
 func NewLogger() kitlog.Logger {
 	logger := kitlog.NewLogfmtLogger(kitlog.NewSyncWriter(os.Stdout))
 	return kitlog.With(logger, "service", version.BinaryName, "ts", kitlog.DefaultTimestampUTC)
+}
+
+// FromContext returns a logger instance from the given context. If the logger
+// is not found we return a new unscoped but usable logger.
+func FromContext(ctx context.Context) kitlog.Logger {
+	if logger, ok := ctx.Value(loggerKey).(kitlog.Logger); ok {
+		return logger
+	}
+
+	logger := NewLogger()
+	return kitlog.With(logger, "module", "logger")
+}
+
+// ToContext sets the given logger into a child context which it now returns.
+func ToContext(ctx context.Context, logger kitlog.Logger) context.Context {
+	return context.WithValue(ctx, loggerKey, logger)
 }
