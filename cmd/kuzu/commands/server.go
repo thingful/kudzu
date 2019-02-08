23ffa79740407bed11b2c23ac9315e2ef43a24e1
Sam Mulube <sam@umbrellium.co.uk>
2019-02-08 17:24:54 +0000

    Add verbose flag to server

--- cmd/kuzu/commands/server.go
+++ cmd/kuzu/commands/server.go
@@ -14,11 +14,13 @@ func init() {
 
 	serverCmd.Flags().StringP("addr", "a", "0.0.0.0:3001", "Specify the address to which the server binds")
 	serverCmd.Flags().StringP("database-url", "d", "", "Connection string for a PostgreSQL instance")
-	serverCmd.Flags().Int("client-timeout", 5, "HTTP client timeout in seconds")
+	serverCmd.Flags().Int("client-timeout", 10, "HTTP client timeout in seconds")
+	serverCmd.Flags().BoolP("verbose", "v", false, "Boolean flag to enable verbose logging")
 
 	viper.BindPFlag("addr", serverCmd.Flags().Lookup("addr"))
 	viper.BindPFlag("database-url", serverCmd.Flags().Lookup("database-url"))
 	viper.BindPFlag("client-timeout", serverCmd.Flags().Lookup("client-timeout"))
+	viper.BindPFlag("verbose", serverCmd.Flags().Lookup("verbose"))
 }
 
 var serverCmd = &cobra.Command{
@@ -41,10 +43,13 @@ var serverCmd = &cobra.Command{
 			return errors.New("Must provide a non-zero client timeout")
 		}
 
+		verbose := viper.GetBool("verbose")
+
 		a := app.NewApp(&app.Config{
 			Addr:          addr,
 			DatabaseURL:   databaseURL,
 			ClientTimeout: clientTimeout,
+			Verbose:       verbose,
 		})
 
 		return a.Start()
