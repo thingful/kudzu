529cbf5d92de5d100d9308a08158dad98c9e817b
Sam Mulube <sam@umbrellium.co.uk>
2019-02-08 14:14:45 +0000

    Simple script to test user creation

--- build/create_user.sh
+++ build/create_user.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+body="{\"User\":{\"Identifier\":\"$1\",\"Provider\":\"parrot\",\"AccessToken\":\"$2\"}}"
+
 curl -X POST \
-  -d '{"User":{"Identifier":"f2048b96-f349-45a6-99be-ff623557ef31","Provider":"parrot","AccessToken":"y2WqFVltC07ljuNivLOJgXOCmcIEeU7fj8G7ttEUdVNQBt02","RefreshToken":"uIyoYwvI9cODJTg3wyuk6ggjS9YkqdoykjvMMbYnSFNHpIWz"}}' \
+  -d "$body" \
   "http://localhost:3001/user/new"
