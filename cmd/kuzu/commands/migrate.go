c61d8f4b7ee717adf996a88d99187e93a4ffe805
Sam Mulube <sam@umbrellium.co.uk>
2019-02-08 12:06:51 +0000

    Add subcommands to migrate up or down

--- cmd/kuzu/commands/migrate.go
+++ cmd/kuzu/commands/migrate.go
@@ -1,11 +1,32 @@
 package commands
 
 import (
+	"errors"
+
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 
 	"github.com/thingful/kuzu/pkg/logger"
 	"github.com/thingful/kuzu/pkg/postgres"
 )
+
+func init() {
+	rootCmd.AddCommand(migrateCmd)
+	migrateCmd.AddCommand(migrateNewCmd)
+	migrateCmd.AddCommand(migrateUpCmd)
+	migrateCmd.AddCommand(migrateDownCmd)
+
+	migrateCmd.PersistentFlags().StringP("database-url", "d", "", "Connection string for a PostgreSQL instance")
+	viper.BindPFlag("database-url", migrateCmd.PersistentFlags().Lookup("database-url"))
+
+	migrateNewCmd.Flags().String("dir", "pkg/migrations/sql", "The directory into which migration files must be written")
+
+	migrateDownCmd.Flags().Int("steps", 1, "Number of down migration steps to run")
+	migrateDownCmd.Flags().Bool("all", false, "Whether to run all down migrations")
+
+	viper.BindPFlag("steps", migrateDownCmd.Flags().Lookup("steps"))
+	viper.BindPFlag("all", migrateDownCmd.Flags().Lookup("all"))
+}
 
 var migrateCmd = &cobra.Command{
 	Use:   "migrate",
@@ -34,15 +55,61 @@ For example:
 			return err
 		}
 
-		logger := logger.NewLogger()
+		log := logger.NewLogger()
 
-		return postgres.NewMigration(dir, args[0], logger)
+		return postgres.NewMigration(dir, args[0], log)
 	},
 }
 
-func init() {
-	rootCmd.AddCommand(migrateCmd)
-	migrateCmd.AddCommand(migrateNewCmd)
+var migrateUpCmd = &cobra.Command{
+	Use:   "up",
+	Short: "Run all up migrations",
+	Long:  `This command attempts to run all up migrations against Postgres.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		databaseURL := viper.GetString("database-url")
+		if databaseURL == "" {
+			return errors.New("Must provide a database url")
+		}
 
-	migrateNewCmd.Flags().String("dir", "pkg/migrations/sql", "The directory into which migration files must be written")
+		log := logger.NewLogger()
+
+		db, err := postgres.Open(databaseURL)
+		if err != nil {
+			return err
+		}
+
+		return postgres.MigrateUp(db.DB, log)
+	},
+}
+
+var migrateDownCmd = &cobra.Command{
+	Use:   "down",
+	Short: "Run down migrations",
+	Long:  `This command attempts to run down migrations against Postgres.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		databaseURL := viper.GetString("database-url")
+		if databaseURL == "" {
+			return errors.New("Must provide a database url")
+		}
+
+		steps := viper.GetInt("steps")
+		all := viper.GetBool("all")
+
+		if steps == 0 && all == false {
+			return errors.New("Must supply either a number of steps or the all flag")
+		}
+
+		log := logger.NewLogger()
+
+		db, err := postgres.Open(databaseURL)
+		if err != nil {
+			return err
+		}
+
+		if all {
+			return postgres.MigrateDownAll(db.DB, log)
+		}
+
+		return postgres.MigrateDown(db.DB, steps, log)
+	},
 }
