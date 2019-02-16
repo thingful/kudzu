2438fc7496b8b421636d72487c31761d5b82480e
Sam Mulube <sam@umbrellium.co.uk>
2019-02-16 01:59:16 +0000

    Add methods for Post/Patch to our http client

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -3,6 +3,7 @@ package client
 import (
 	"context"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"net/url"
@@ -37,7 +38,6 @@ func NewClient(timeout int, verbose bool) *Client {
 
 // Get attempts to fetch the given URL, setting the correct authorization and
 // user agent header
-// TODO: add context to pass in logger
 func (c *Client) Get(ctx context.Context, requestURL, accessToken string) ([]byte, error) {
 	log := logger.FromContext(ctx)
 
@@ -58,6 +58,10 @@ func (c *Client) Get(ctx context.Context, requestURL, accessToken string) ([]byt
 
 	resp, err := c.client.Do(req)
 	if err != nil {
+		log.Log(
+			"msg", "error making request",
+			"error", err,
+		)
 		if err.(*url.Error).Timeout() {
 			return nil, TimeoutError
 		}
@@ -68,6 +72,71 @@ func (c *Client) Get(ctx context.Context, requestURL, accessToken string) ([]byt
 
 	if resp.StatusCode != http.StatusOK {
 		log.Log("msg", "unexpected response code", "code", resp.StatusCode)
+		switch resp.StatusCode {
+		case http.StatusUnauthorized:
+			return nil, UnauthorizedError
+		case http.StatusNotFound:
+			return nil, NotFoundError
+		default:
+			return nil, fmt.Errorf("Unexpected response: %s", resp.Status)
+		}
+	}
+
+	return ioutil.ReadAll(resp.Body)
+}
+
+// Post attempts to send a JSON POST request to the given URL, setting the
+// correct authorization header and content type. Returns an error on any
+// failure, or a slice of bytes.
+func (c *Client) Post(ctx context.Context, requestURL, accessToken string, body io.Reader) ([]byte, error) {
+	return c.postOrPatch(ctx, http.MethodPost, requestURL, accessToken, body)
+}
+
+// Patch attempts to send a JSON PATCH request to the given URL, setting the
+// correct authorization header and content type. Returns an error on any
+// failure, or a slice of bytes.
+func (c *Client) Patch(ctx context.Context, requestURL, accessToken string, body io.Reader) ([]byte, error) {
+	return c.postOrPatch(ctx, http.MethodPatch, requestURL, accessToken, body)
+}
+
+func (c *Client) postOrPatch(ctx context.Context, method, requestURL, accessToken string, body io.Reader) ([]byte, error) {
+	log := logger.FromContext(ctx)
+
+	if c.verbose {
+		log.Log(
+			"msg", "posting to url",
+			"url", requestURL,
+		)
+	}
+
+	req, err := http.NewRequest(method, requestURL, body)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create http request")
+	}
+
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", accessToken))
+	req.Header.Set("User-Agent", c.userAgent)
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		log.Log(
+			"msg", "error making request",
+			"error", err,
+		)
+		if err.(*url.Error).Timeout() {
+			return nil, TimeoutError
+		}
+
+		return nil, UnexpectedError
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusCreated {
+		log.Log(
+			"msg", "unexpected response code",
+			"code", resp.StatusCode,
+		)
 		switch resp.StatusCode {
 		case http.StatusUnauthorized:
 			return nil, UnauthorizedError
