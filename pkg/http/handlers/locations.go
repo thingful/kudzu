1f48b4572552a0844578dfab4a7877f106ccc714
Sam Mulube <sam@umbrellium.co.uk>
2019-02-17 13:55:46 +0000

    Mux in the location update handler

--- pkg/http/handlers/locations.go
+++ pkg/http/handlers/locations.go
@@ -15,9 +15,10 @@ import (
 // RegisterLocationHandlers registers handlers for working with locations
 func RegisterLocationHandlers(mux *goji.Mux, db *postgres.DB) {
 	mux.Handle(pat.Post("/entity/locations/get"), Handler{env: &Env{db: db}, handler: listLocationsHandler})
-	//mux.Handle(pat.Post("/entity/locations/update"), Handler{env: &Env{db: db}, handler: updateLocationHandler})
+	mux.Handle(pat.Post("/entity/locations/update"), Handler{env: &Env{db: db}, handler: updateLocationHandler})
 }
 
+// listLocationsRequest is used to parse incoming requests for locations
 type listLocationsRequest struct {
 	UserUID         string   `json:"UserId"`
 	DataSourceCodes []string `json:"DataSourceCodes"`
@@ -25,6 +26,16 @@ type listLocationsRequest struct {
 	StaleData       bool     `json:"StaleData"`
 }
 
+// updateLocationRequest is used to parse incoming requests to set the location
+// of a device
+type updateLocationRequest struct {
+	Code string  `json:"Code"`
+	X    float64 `json:"X"`
+	Y    float64 `json:"Y"`
+}
+
+// location is used when rendering the response to the client. The structure is
+// defined by hydronet
 type location struct {
 	Code                         string  `json:"Code"`
 	DataSourceGroupCode          string  `json:"DataSourceGroupCode"`
@@ -40,6 +51,8 @@ type location struct {
 	LastFetchedSampleTimestamp   string  `json:"LastFetchedSampleTimestamp"`
 }
 
+// listLocationsHandler is our handler that returns location information to
+// clients
 func listLocationsHandler(env *Env, w http.ResponseWriter, r *http.Request) error {
 	ctx := r.Context()
 
@@ -56,29 +69,15 @@ func listLocationsHandler(env *Env, w http.ResponseWriter, r *http.Request) erro
 		}
 	}
 
-	locationMap := map[string]location{}
+	locationMap := map[string]*location{}
 
 	for _, loc := range locations {
-		l := location{
-			Code:                         loc.UID,
-			DataSourceGroupCode:          "Grow.Thingful",
-			Identifier:                   fmt.Sprintf("Grow.Thingful#%s", loc.UID),
-			Name:                         fmt.Sprintf("%s. Serial Number: %s", loc.Nickname, loc.SerialNum),
-			LocationID:                   loc.ID,
-			ProjectionID:                 3,
-			X:                            loc.Longitude,
-			Y:                            loc.Latitude,
-			Z:                            0,
-			FirstSampleTimestamp:         loc.FirstSampleUTC.Time.Format("20060102150405"),
-			LastAvailableSampleTimestamp: loc.LastSampleUTC.Time.Format("20060102150405"),
-			LastFetchedSampleTimestamp:   loc.LastUploadedSampleUTC.Time.Format("20060102150405"),
-		}
-
+		l := buildLocation(&loc)
 		locationMap[fmt.Sprintf("Grow.Thingful#%s", loc.UID)] = l
 	}
 
 	b, err := json.Marshal(struct {
-		Locations map[string]location `json:"Locations"`
+		Locations map[string]*location `json:"Locations"`
 	}{
 		Locations: locationMap,
 	})
@@ -114,4 +113,77 @@ func parseListRequest(r *http.Request) (*listLocationsRequest, error) {
 	}
 
 	return &data, nil
+}
+
+func updateLocationHandler(env *Env, w http.ResponseWriter, r *http.Request) error {
+	ctx := r.Context()
+
+	req, err := parseUpdateRequest(r)
+	if err != nil {
+		return err
+	}
+
+	loc, err := env.db.UpdateGeolocation(ctx, req.Code, req.X, req.Y)
+	if err != nil {
+		return &HTTPError{
+			Code: http.StatusInternalServerError,
+			Err:  errors.Wrap(err, "failed to update geolocation"),
+		}
+	}
+
+	output := buildLocation(loc)
+
+	b, err := json.Marshal(output)
+	if err != nil {
+		return &HTTPError{
+			Code: http.StatusInternalServerError,
+			Err:  errors.Wrap(err, "failed to marshal response JSON"),
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(b)
+
+	return nil
+}
+
+// parseUpdateRequest builds a updateLocationRequest object or returns an error
+func parseUpdateRequest(r *http.Request) (*updateLocationRequest, error) {
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return nil, &HTTPError{
+			Code: http.StatusInternalServerError,
+			Err:  errors.Wrap(err, "failed to read incoming request body"),
+		}
+	}
+
+	var data updateLocationRequest
+	err = json.Unmarshal(b, &data)
+	if err != nil {
+		return nil, &HTTPError{
+			Code: http.StatusUnprocessableEntity,
+			Err:  errors.Wrap(err, "failed to parse incoming request body"),
+		}
+	}
+
+	return &data, nil
+}
+
+// buildLocation builds our output location type from the location returned from
+// Postgres
+func buildLocation(loc *postgres.Location) *location {
+	return &location{
+		Code:                         loc.UID,
+		DataSourceGroupCode:          "Grow.Thingful",
+		Identifier:                   fmt.Sprintf("Grow.Thingful#%s", loc.UID),
+		Name:                         fmt.Sprintf("%s. Serial Number: %s", loc.Nickname, loc.SerialNum),
+		LocationID:                   loc.ID,
+		ProjectionID:                 3,
+		X:                            loc.Longitude,
+		Y:                            loc.Latitude,
+		Z:                            0,
+		FirstSampleTimestamp:         loc.FirstSampleUTC.Time.Format("20060102150405"),
+		LastAvailableSampleTimestamp: loc.LastSampleUTC.Time.Format("20060102150405"),
+		LastFetchedSampleTimestamp:   loc.LastUploadedSampleUTC.Time.Format("20060102150405"),
+	}
 }
