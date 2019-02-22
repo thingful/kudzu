4df6d42f1ac82fae01d509ebe9540720d6fbb285
Sam Mulube <sam@umbrellium.co.uk>
2019-02-22 14:01:59 +0000

    Move junk stuff out of everywhere and dump in a tmp folder

--- build/create_user.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-body="{\"User\":{\"Identifier\":\"$1\",\"Provider\":\"parrot\",\"AccessToken\":\"$2\"}}"
-
-curl -X POST \
-  -d "$body" \
-  "http://localhost:3001/user/new"
